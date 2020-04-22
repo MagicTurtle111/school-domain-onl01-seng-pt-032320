b6b1c67caba681351ade2ac5feb9f56f036b1199
Joshua Williams <j.o-williams@outlook.com>
2020-04-22 16:09:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class Song
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all.each do |song|
+      puts song.name
+    end
+  end
+end
+
+sik_k = Song.new("rendevouz")
+uh = Song.new("huh")
+# sik_k.name
+Song.all
+
+ 
