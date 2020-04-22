fdfa59bb753db8233a9b1106781ccc237c3f1732
Joshua Williams <j.o-williams@outlook.com>
2020-04-22 16:21:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,40 +1,22 @@
- class School
-   attr_accessor :name, :roster
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
+  end
   
-   def initialize(name)
-     @name = name
-     @roster = {}
-   end 
+  def roster
+  	roster = {}
+  end 
   
-   def add_student(student, level)
-#     # this sort of means #=> a || a = b ... or #=> x || x = y
-#     # it's freak'n weird
-     roster[level] ||= []
-     roster[level] << student
-   end 
-  
-   def grade(level)
-     roster.detect do |x, y| 
-       if x == level
-         return y 
-       end 
-     end 
-   end 
- end
- 
-  
-   school = School.new("Bayside High School")
- school.roster
- school.add_student("Zach Morris", 9)
- school.add_student("Moshe Zeus", 9)
- school.add_student("Onkelos Octavius", 9)
- school.roster
- school.add_student("Sugar Zaza", 5)
- school.roster
- school.add_student("Theo Madus", 5)
- school.add_student("Abbah Zabbah", 2)
- school.roster
- school.grade(9)
- school.sort
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
 
-  
+end
+
+
+School.new("R").roster.add_student("john",9)
