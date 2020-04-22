2c4a7000c8c3d16657e459444ab6586f5b505320
Joshua Williams <j.o-williams@outlook.com>
2020-04-22 16:18:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,13 +22,6 @@
    end 
  end
  
- def sort 
-   nu_hash = {}
-   roster.each do |x, y| 
-     nu_hash[x] = y.sort 
-   end 
-   nu_hash
- end 
   
    school = School.new("Bayside High School")
  school.roster
